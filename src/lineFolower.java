cef4902c2d34b3d2d3049eb5c7747164790ab894
mattijn <m.kreuzen@student.ru.nl>
2015-05-22 14:25:08 +0200

    tfgyuktuikgtujl,ghulij

--- src/lineFolower.java
+++ src/lineFolower.java
@@ -5,8 +5,8 @@ public interface lineFolower {
 	
 	
 	public ArrayList<float[]> sweep( int degrees, int degrees_sweep, boolean toleft, int speed);
-	public int move_arm_to_position();
 	
+	public int move_arm_to_position();
 	public void follow_single_line();
 	
 	
--- src/movement.java
+++ src/movement.java
@@ -1,6 +1,11 @@
 
 public interface movement {
+	/* gebruikt rotate om naar de huidige positie van de arm te draaien. 
+	 * en ze de arme recht naar voren*/
 	public void move_according_to_arm(int old_position);
+	/* gebruikt de gyroscopp om te draaien*/
 	public void rotate(int degrees);
+	/* een algoritme dat de robot voorzichtig 
+	 * over een wiwap laat rijden*/
 	public void beweeg_over_wip_wap();
 }

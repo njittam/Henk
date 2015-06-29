cd9a882eeb68588b0ee9d41817410acb58a94e80
DrDoomtastic <DrDoomtastic@Huehuehuehue>
2015-06-29 13:59:23 +0200

    rotate werkt niet

--- src/numberofdegrees.java
+++ src/numberofdegrees.java
@@ -9,16 +9,17 @@ import lejos.utility.Delay;
 
 
 public class numberofdegrees {
-	public NXTLightSensor sensor;
-	public SampleProvider sp;
+	public NXTLightSensor sensor1;
+	//public SampleProvider sp;
 	public numberofdegrees(NXTLightSensor s){
-		this.sensor = s;
-		sp = sensor.getRedMode();
+		this.sensor1 = s;
+		//sp = sensor.getRedMode();
+		this.sensor1.close();
 	}
 	
 
 	public boolean find_path () {
-		NXTRegulatedMotor arm = Motor.D;
+		/*NXTRegulatedMotor arm = Motor.D;
 		int middle_positon = 0;
 		int minpos = middle_positon - 70;
 		int maxpos = middle_positon + 70;
@@ -45,7 +46,7 @@ public class numberofdegrees {
 			}
 		}
 		arm.stop(true); v b*/
-		
+		/*
 		arm.setSpeed(armspeed);
 		arm.rotateTo(minpos);   
 		while (!klaar){
@@ -59,6 +60,39 @@ public class numberofdegrees {
 		if (!klaar)
 			arm.rotateTo(middle_positon);
 		arm.stop(true);
-		return klaar;
-	}	
+		return klaar;*/
+		NXTRegulatedMotor arm = Motor.D;
+		int middle_positon = 0;
+		NXTLightSensor sensor = new NXTLightSensor(SensorPort.S2);
+		int minpos = middle_positon - 90;
+		int maxpos = middle_positon + 90;
+		double pad_ambient = 0.45; // waarde tussen 1 en 0
+		int armspeed = 100;
+		arm.rotateTo(minpos);
+		//sensor.setFloodlight(true);
+		boolean klaar = false;
+		arm.setSpeed(armspeed);
+		SampleProvider sp = sensor.getRedMode();
+		float[] sample = new float[sp.sampleSize()];
+		arm.forward();// TODO forward of backward?
+		while (!klaar){
+			//sensor.setFloodlight(true);
+	        sp.fetchSample(sample, 0);
+			System.out.println(sample[0]);
+			if (sample[0] > pad_ambient || arm.getTachoCount() >= maxpos ) {
+				arm.stop();
+				klaar = true;
+			}
+		}
+		System.out.println("hallo dit is lijn 87");
+		System.out.println(arm.getTachoCount());
+		System.out.println(klaar);
+		arm.stop();
+		if (arm.getTachoCount() >= maxpos)
+			arm.rotateTo(middle_positon);
+		System.out.println("juhgfced");
+		sensor.close();
+		return false;
+	}
+	
 }

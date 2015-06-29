a84837e52c6fde2d10e263b6cb00d843caef6a70
s4437403 <s4437403@13-01424.ru.nl>
2015-06-29 14:08:32 +0200

    siuoudks and towersid sdetwctianods

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,26 @@
+import lejos.robotics.Color;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.NXTLightSensor;
 
-
 public class Main {
 
-	public static  void main(String[] args) {
+	public static void main(String[] args) {
+		detection det = new detection();
 		Sounds sounds = new Sounds();
-		if () {
-			sounds.geluid();
+		int i= 0;
+		while (i < 5000000) {
+			System.out.println("TEST1");
+			if (det.detect_Tower() == Color.BLUE) {
+				sounds.geluid();
+			}
+			i  += 1;
 		}
 		Motor.D.resetTachoCount();
-		LineFollower lf = new LineFollower(new EV3GyroSensor(SensorPort.S1),new NXTLightSensor(SensorPort.S2));
+		LineFollower lf = new LineFollower(new EV3GyroSensor(SensorPort.S1),
+				new NXTLightSensor(SensorPort.S2));
 		lf.line_follower(new linetest());
 	}
-	
-	
+
 }
-		

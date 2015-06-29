f0004feb88e83e572635b04db9c83adce3c73b2c
s4437403 <s4437403@13-01424.ru.nl>
2015-06-29 10:27:41 +0200

    ew

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
-import java.util.ArrayList;
-
-import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.Motor;
 import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.NXTLightSensor;
-import lejos.hardware.sensor.SensorMode;
-import lejos.utility.Delay;
 
 
 public class Main {
 
 	public static  void main(String[] args) {
+		Sounds sounds = new Sounds();
+		if () {
+			sounds.geluid();
+		}
 		Motor.D.resetTachoCount();
 		LineFollower lf = new LineFollower(new EV3GyroSensor(SensorPort.S1),new NXTLightSensor(SensorPort.S2));
 		lf.line_follower(new linetest());

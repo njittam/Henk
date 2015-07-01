b47ab95d68d77a52d6c08cd0d354c99fb422f158
DrDoomtastic <DrDoomtastic@Huehuehuehue>
2015-07-01 14:40:23 +0200

    Obstacles

---
+++ src/Obstacles.java
@@ -0,0 +1,23 @@
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.NXTLightSensor;
+
+public class Obstacles {
+	public EV3GyroSensor gyro;
+	public NXTLightSensor light;
+	public EV3ColorSensor color;
+	public EV3UltrasonicSensor eyes;
+	public Obstacles(EV3GyroSensor gyro, NXTLightSensor light, 
+			EV3UltrasonicSensor eyes, EV3ColorSensor color) {
+		this.gyro = gyro;
+		this.light = light;
+		this.color = color;
+		this.eyes = eyes;
+	}
+
+	public void Bridge() {
+		
+	}
+
+}
